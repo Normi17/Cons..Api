3f9dad20e5c6009c65d4d5c422f2258e33523db5
Normi17 <carlosgaraynormila@gmail.com>
2023-11-03 11:14:06 -0500

    nav

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,18 +1,56 @@
 import { NavLink } from "react-router-dom";
 import styled from 'styled-components';
 
+const Nav = styled.nav`
+  background-color: #333; /* Color de fondo */
+  color: #fff; /* Color del texto */
+  padding: 10px 0;
+`;
 
-export function Navbar(){
-    return(
-        <nav >
-            <ul>
-                <NavLink to='/fake'><li>Fake Store</li></NavLink>
-                <NavLink to='/poke'><li>Poke</li></NavLink>
-                <NavLink to='/rick'><li>Ricky</li></NavLink>
-                <NavLink to='/movie'><li>MTBMovie</li></NavLink>
-                <NavLink to='/marvel'><li>Marvel</li></NavLink>
-            </ul>
-        </nav>
-        
-    )
+const NavList = styled.ul`
+  list-style: none;
+  display: flex;
+  justify-content: space-around;
+  padding: 0;
+`;
+
+const NavItem = styled.li`
+  font-size: 18px;
+  text-transform: uppercase;
+  padding: 5px 10px;
+  border-radius: 5px;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: #ff5900; /* Cambio de color al pasar el cursor */
+  }
+`;
+
+const StyledNavLink = styled(NavLink)`
+  text-decoration: none;
+  color: white;
+`;
+
+export function Navbar() {
+  return (
+    <Nav>
+      <NavList>
+        <StyledNavLink to='/fake'>
+          <NavItem>Fake Store</NavItem>
+        </StyledNavLink>
+        <StyledNavLink to='/poke'>
+          <NavItem>Poke</NavItem>
+        </StyledNavLink>
+        <StyledNavLink to='/rick'>
+          <NavItem>Ricky</NavItem>
+        </StyledNavLink>
+        <StyledNavLink to='/movie'>
+          <NavItem>MTBMovie</NavItem>
+        </StyledNavLink>
+        <StyledNavLink to='/marvel'>
+          <NavItem>Marvel</NavItem>
+        </StyledNavLink>
+      </NavList>
+    </Nav>
+  );
 }
